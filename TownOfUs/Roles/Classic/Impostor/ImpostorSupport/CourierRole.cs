75eafee5438dc9f1382ddef6c00fde26a71a3947
clpb <caiquebughi@gmail.com>
2026-06-07 10:43:51 -0300

    Add Courier

---
+++ TownOfUs/Roles/Classic/Impostor/ImpostorSupport/CourierRole.cs
@@ -0,0 +1,499 @@
+﻿using AmongUs.GameOptions;
+using Il2CppInterop.Runtime.Attributes;
+using MiraAPI.Events;
+using MiraAPI.GameOptions;
+using MiraAPI.Hud;
+using MiraAPI.Modifiers;
+using MiraAPI.Roles;
+using MiraAPI.Utilities;
+using Reactor.Networking.Attributes;
+using Reactor.Networking.Rpc;
+using TownOfUs.Buttons.Impostor;
+using TownOfUs.Events.TouEvents;
+using TownOfUs.Modifiers.Impostor;
+using TownOfUs.Options.Roles.Impostor;
+using TownOfUs.Roles.Crewmate;
+using UnityEngine;
+
+namespace TownOfUs.Roles.Impostor;
+
+public sealed class CourierRole(IntPtr cppPtr)
+    : ImpostorRole(cppPtr), ITownOfUsRole, IWikiDiscoverable, IDoomable, ICrewVariant
+{
+    public void FixedUpdate()
+    {
+        if (!Player || Player.Data.Role is not CourierRole || Player.HasDied() || !Player.AmOwner ||
+            MeetingHud.Instance || (!HudManager.Instance.UseButton.isActiveAndEnabled &&
+                                    !HudManager.Instance.PetButton.isActiveAndEnabled))
+        {
+            return;
+        }
+
+        HudManager.Instance.KillButton.ToggleVisible(OptionGroupSingleton<CourierOptions>.Instance.CourierKill ||
+                                                     (Player != null && Player.GetModifiers<BaseModifier>()
+                                                         .Any(x => x is ICachedRole)) ||
+                                                     (Player != null && MiscUtils.ImpAliveCount == 1));
+    }
+
+    public RoleBehaviour CrewVariant => RoleManager.Instance.GetRole((RoleTypes)RoleId.Get<TransporterRole>());
+    public DoomableType DoomHintType => DoomableType.Fearmonger;
+    public string LocaleKey => "Courier";
+    public string RoleName => TouLocale.Get($"TouRole{LocaleKey}");
+    public string RoleDescription => TouLocale.GetParsed($"TouRole{LocaleKey}IntroBlurb");
+    public string RoleLongDescription => TouLocale.GetParsed($"TouRole{LocaleKey}TabDescription");
+
+    public string GetAdvancedDescription()
+    {
+        return
+            TouLocale.GetParsed($"TouRole{LocaleKey}WikiDescription") +
+            MiscUtils.AppendOptionsText(GetType());
+    }
+
+    public Color RoleColor => TownOfUsColors.Impostor;
+    public ModdedRoleTeams Team => ModdedRoleTeams.Impostor;
+    public RoleAlignment RoleAlignment => RoleAlignment.ImpostorSupport;
+
+    public CustomRoleConfiguration Configuration => new(this)
+    {
+        UseVanillaKillButton = true,
+        CanUseVent = OptionGroupSingleton<CourierOptions>.Instance.CanVent,
+        Icon = TouRoleIcons.Transporter,
+        OptionsScreenshot = TouBanners.ImpostorRoleBanner,
+    };
+
+
+
+    [HideFromIl2Cpp]
+    public List<CustomButtonWikiDescription> Abilities
+    {
+        get
+        {
+            return new List<CustomButtonWikiDescription>
+            {
+                new(TouLocale.GetParsed($"TouRole{LocaleKey}Transport", "Transport"),
+                    TouLocale.GetParsed($"TouRole{LocaleKey}TransportWikiDescription"),
+                    TouCrewAssets.Transport)
+            };
+        }
+    }
+
+    [MethodRpc((uint)TownOfUsRpc.Transport)]
+    public static void RpcTransport(PlayerControl transporter, byte player1, byte player2)
+    {
+        if (LobbyBehaviour.Instance)
+        {
+            MiscUtils.RunAnticheatWarning(transporter);
+            return;
+        }
+        if (transporter.Data.Role is not TransporterRole)
+        {
+            Error("RpcTransport - Invalid Transporter");
+            return;
+        }
+
+        var t1 = GetTarget(player1);
+        var t2 = GetTarget(player2);
+
+        //also check again incase they went on the usable while the transporter was picking but ignore vents
+        if (t1 == null || t2 == null)
+        {
+            if (transporter.AmOwner)
+            {
+                Coroutines.Start(MiscUtils.CoFlash(Color.red));
+            }
+
+            return;
+        }
+
+        var play1 = MiscUtils.PlayerById(player1)!;
+        var play2 = MiscUtils.PlayerById(player2)!;
+
+        var warden = play1.GetModifier<WardenFortifiedModifier>()?.Warden.GetRole<WardenRole>();
+        if (warden != null)
+        {
+            if (transporter.AmOwner)
+            {
+                WardenRole.RpcWardenNotify(warden.Player, transporter, play1);
+            }
+
+            return;
+        }
+
+        var warden2 = play2.GetModifier<WardenFortifiedModifier>()?.Warden.GetRole<WardenRole>();
+        if (warden2 != null)
+        {
+            if (transporter.AmOwner)
+            {
+                WardenRole.RpcWardenNotify(warden2.Player, transporter, play2);
+            }
+
+            return;
+        }
+
+        var cleric = play1.GetModifier<ClericBarrierModifier>()?.Cleric.GetRole<ClericRole>();
+        if (cleric != null)
+        {
+            if (transporter.AmOwner)
+            {
+                ClericRole.RpcClericBarrierAttacked(transporter, cleric.Player, play1);
+            }
+
+            return;
+        }
+
+        var cleric2 = play2.GetModifier<ClericBarrierModifier>()?.Cleric.GetRole<ClericRole>();
+        if (cleric2 != null)
+        {
+            if (transporter.AmOwner)
+            {
+                ClericRole.RpcClericBarrierAttacked(transporter, cleric2.Player, play2);
+            }
+
+            return;
+        }
+
+        var infectedtrans = transporter.GetModifier<PlaguebearerInfectedModifier>();
+        var infectedplayer1 = play1.GetModifier<PlaguebearerInfectedModifier>();
+        var infectedplayer2 = play2.GetModifier<PlaguebearerInfectedModifier>();
+        if (infectedtrans != null)
+        {
+            if (infectedplayer1 == null)
+            {
+                play1.AddModifier<PlaguebearerInfectedModifier>(infectedtrans.PlagueBearerId);
+            }
+
+            if (infectedplayer2 == null)
+            {
+                play2.AddModifier<PlaguebearerInfectedModifier>(infectedtrans.PlagueBearerId);
+            }
+        }
+        else if (infectedtrans == null && infectedplayer1 != null)
+        {
+            transporter.AddModifier<PlaguebearerInfectedModifier>(infectedplayer1.PlagueBearerId);
+        }
+        else if (infectedtrans == null && infectedplayer2 != null)
+        {
+            transporter.AddModifier<PlaguebearerInfectedModifier>(infectedplayer2.PlagueBearerId);
+        }
+
+        LookoutEvents.CheckForLookoutWatched(transporter, play1);
+        LookoutEvents.CheckForLookoutWatched(transporter, play2);
+
+        var mercenary = PlayerControl.LocalPlayer.Data.Role as MercenaryRole;
+        if ((play1.HasModifier<MercenaryGuardModifier>() || play2.HasModifier<MercenaryGuardModifier>()) && mercenary)
+        {
+            mercenary!.AddPayment();
+        }
+
+        if (play1.TryGetModifier<InvulnerabilityModifier>(out var invic) && invic.AttackAllInteractions)
+        {
+            if (transporter.AmOwner)
+            {
+                play1.RpcCustomMurder(transporter, MeetingCheck.OutsideMeeting);
+            }
+
+            return;
+        }
+
+        if (play2.TryGetModifier<InvulnerabilityModifier>(out var invic2) && invic2.AttackAllInteractions)
+        {
+            if (transporter.AmOwner)
+            {
+                play2.RpcCustomMurder(transporter, MeetingCheck.OutsideMeeting);
+            }
+
+            return;
+        }
+
+        if (play1.HasModifier<VeteranAlertModifier>())
+        {
+            if (transporter.AmOwner)
+            {
+                play1.RpcCustomMurder(transporter, MeetingCheck.OutsideMeeting);
+            }
+
+            return;
+        }
+
+        if (play2.HasModifier<VeteranAlertModifier>())
+        {
+            if (transporter.AmOwner)
+            {
+                play2.RpcCustomMurder(transporter, MeetingCheck.OutsideMeeting);
+            }
+
+            return;
+        }
+
+        if (play1.TryGetModifier<ShyModifier>(out var shy))
+        {
+            shy.OnRoundStart();
+        }
+
+        if (play2.TryGetModifier<ShyModifier>(out var shy2))
+        {
+            shy2.OnRoundStart();
+        }
+
+        if (t1.TryCast<DeadBody>())
+        {
+            PreCheckUndertaker(t1.TryCast<DeadBody>()!);
+        }
+
+        if (t2.TryCast<DeadBody>())
+        {
+            PreCheckUndertaker(t2.TryCast<DeadBody>()!);
+        }
+
+        var positions = GetAdjustedPositions(t1, t2);
+        if (t1.TryCast<PlayerControl>() != null && t2.TryCast<DeadBody>() != null)
+        {
+            positions.Item1 = play1.Collider.bounds.center;
+        }
+
+        if (t2.TryCast<PlayerControl>() != null && t1.TryCast<DeadBody>() != null)
+        {
+            positions.Item2 = play2.Collider.bounds.center;
+        }
+
+        Transport(t1, positions.Item2);
+        Transport(t2, positions.Item1);
+        var touAbilityEvent = new TouAbilityEvent(AbilityType.TransporterTransport, transporter, t1, t2);
+        MiraEventManager.InvokeEvent(touAbilityEvent);
+
+        if (transporter.AmOwner)
+        {
+            var button = CustomButtonSingleton<TransporterTransportButton>.Instance;
+            button.DecreaseUses();
+            button.ResetCooldownAndOrEffect();
+
+            TownOfUsColors.UseBasic = false;
+            if (button.TextOutlineColor != Color.clear)
+            {
+                button.SetTextOutline(button.TextOutlineColor);
+                if (button.Button != null)
+                {
+                    button.Button.usesRemainingSprite.color = button.TextOutlineColor;
+                }
+            }
+
+            TownOfUsColors.UseBasic = LocalSettingsTabSingleton<TownOfUsLocalRoleSettings>.Instance
+                .UseCrewmateTeamColorToggle.Value;
+        }
+
+        if (play1.AmOwner && t1 is PlayerControl || play2.AmOwner && t2 is PlayerControl)
+        {
+            var notif1 = Helpers.CreateAndShowNotification(
+                $"<b>{TownOfUsColors.Transporter.ToTextColor()}{TouLocale.GetParsed("TouRoleTransporterTransportNotif")}</color></b>", Color.white,
+                new Vector3(0f, 1f, -20f), spr: TouRoleIcons.Transporter.LoadAsset());
+
+            notif1.AdjustNotification();
+
+            if (Minigame.Instance)
+            {
+                Minigame.Instance.Close();
+                Minigame.Instance.Close();
+            }
+        }
+
+        MonoBehaviour? GetTarget(byte id)
+        {
+            var data = GameData.Instance.GetPlayerById(id);
+            if (!data)
+            {
+                return null;
+            }
+
+            var body = Helpers.GetBodyById(id);
+            if (data.IsDead && body)
+            {
+                return body;
+            }
+
+            var pc = data.Object;
+            if (!pc)
+            {
+                return null;
+            }
+
+            if (pc.HasModifier<NoTransportModifier>())
+            {
+                return null;
+            }
+
+            if (pc.GetModifiers<BaseModifier>().Any(x => x is IUntransportable))
+            {
+                return null;
+            }
+            // FAKE PLAYER MUST BE A MONO BEHAVIOUR FOR THIS TO WORK
+            /*var fakePlayer = Utilities.MiscUtils.GetFakePlayer(pc)?.body;
+
+            if (fakePlayer)
+            {
+                return fakePlayer;
+            } */
+
+            if (pc.moveable || pc.inVent || (pc.TryGetModifier<DisabledModifier>(out var mod) &&
+                                             (!mod.IsConsideredAlive || !mod.CanBeInteractedWith)))
+            {
+                if (pc.inVent)
+                {
+                    pc.MyPhysics.ExitAllVents();
+                }
+
+                return pc;
+            }
+
+            return null;
+        }
+
+        void PreCheckUndertaker(DeadBody body)
+        {
+            var mods = ModifierUtils.GetActiveModifiers<DragModifier>();
+
+            foreach (var mod in mods)
+            {
+                if (mod.BodyId == body.ParentId)
+                {
+                    var dragMod = mod.Player.GetModifier<DragModifier>()!;
+                    var dropPos = body.transform.position;
+                    dropPos.z = dropPos.y / 1000f;
+                    dragMod.DeadBody!.transform.position = dropPos;
+
+                    var touAbilityEvent2 = new TouAbilityEvent(AbilityType.UndertakerDrop, mod.Player, dragMod.DeadBody);
+                    MiraEventManager.InvokeEvent(touAbilityEvent2);
+
+                    if (mod.Player.AmOwner)
+                    {
+                        CustomButtonSingleton<UndertakerDragDropButton>.Instance.SetDrag();
+                    }
+
+                    mod.Player.RemoveModifier(dragMod);
+                }
+            }
+        }
+
+        (Vector2, Vector2) GetAdjustedPositions(MonoBehaviour transportable, MonoBehaviour transportable2)
+        {
+            // assign dummy values so it doesnt error about returning unassigned variables
+            Vector2 TP1Position = new(0, 0);
+            Vector2 TP2Position = new(0, 0);
+
+            if (transportable.TryCast<DeadBody>() == null && transportable2.TryCast<DeadBody>() == null)
+            {
+                Error($"type: {transportable.GetIl2CppType().Name}");
+                var TP1 = transportable.TryCast<PlayerControl>()!;
+                TP1Position = TP1.GetTruePosition();
+                TP1Position = new Vector2(TP1Position.x, TP1Position.y + 0.3636f);
+
+                var TP2 = transportable2.TryCast<PlayerControl>()!;
+                TP2Position = TP2.GetTruePosition();
+                TP2Position = new Vector2(TP2Position.x, TP2Position.y + 0.3636f);
+
+                if (TP1.HasModifier<MiniModifier>())
+                {
+                    TP1Position = new Vector2(TP1Position.x, TP1Position.y + 0.2233912f * 0.75f);
+                    TP2Position = new Vector2(TP2Position.x, TP2Position.y - 0.2233912f * 0.75f);
+                }
+                else if (TP2.HasModifier<MiniModifier>())
+                {
+                    TP1Position = new Vector2(TP1Position.x, TP1Position.y - 0.2233912f * 0.75f);
+                    TP2Position = new Vector2(TP2Position.x, TP2Position.y + 0.2233912f * 0.75f);
+                }
+            }
+            else if (transportable.TryCast<DeadBody>() != null && transportable2.TryCast<DeadBody>() == null)
+            {
+                var Player1Body = transportable.TryCast<DeadBody>()!;
+                TP1Position = Player1Body.TruePosition;
+                TP1Position = new Vector2(TP1Position.x, TP1Position.y + 0.3636f);
+
+                var TP2 = transportable2.TryCast<PlayerControl>()!;
+                TP2Position = TP2.GetTruePosition();
+                TP2Position = new Vector2(TP2Position.x, TP2Position.y + 0.3636f);
+
+                if (TP2.HasModifier<MiniModifier>())
+                {
+                    TP1Position = new Vector2(TP1Position.x, TP1Position.y - 0.2233912f * 0.75f);
+                    TP2Position = new Vector2(TP2Position.x, TP2Position.y + 0.2233912f * 0.75f);
+                }
+            }
+            else if (transportable.TryCast<DeadBody>() == null && transportable2.TryCast<DeadBody>() != null)
+            {
+                var TP1 = transportable.TryCast<PlayerControl>()!;
+                TP1Position = TP1.GetTruePosition();
+                TP1Position = new Vector2(TP1Position.x, TP1Position.y + 0.3636f);
+
+                var Player2Body = transportable2.TryCast<DeadBody>()!;
+                TP2Position = Player2Body.TruePosition;
+                TP2Position = new Vector2(TP2Position.x, TP2Position.y + 0.3636f);
+                if (TP1.HasModifier<MiniModifier>())
+                {
+                    TP1Position = new Vector2(TP1Position.x, TP1Position.y + 0.2233912f * 0.75f);
+                    TP2Position = new Vector2(TP2Position.x, TP2Position.y - 0.2233912f * 0.75f);
+                }
+            }
+            else if (transportable.TryCast<DeadBody>() != null && transportable2.TryCast<DeadBody>() != null)
+            {
+                TP1Position = transportable.TryCast<DeadBody>()!.TruePosition;
+                TP2Position = transportable2.TryCast<DeadBody>()!.TruePosition;
+            }
+
+            return (TP1Position, TP2Position);
+        }
+    }
+
+    public static void Transport(MonoBehaviour mono, Vector3 position)
+    {
+        var deadBody = mono.TryCast<DeadBody>();
+        var player = mono.TryCast<PlayerControl>();
+        if (player != null && player.HasModifier<ImmovableModifier>())
+        {
+            return;
+        }
+
+        if (deadBody != null &&
+            MiscUtils.PlayerById(deadBody.ParentId)?.HasModifier<ImmovableModifier>() == true)
+        {
+            return;
+        }
+
+        if (player != null)
+        {
+            player.MyPhysics.ResetMoveState();
+            player.transform.position = position;
+            player.NetTransform.SnapTo(position);
+        }
+
+        mono.transform.position = position;
+        Collider2D cd = mono.GetComponent<Collider2D>();
+        if (cd != null && deadBody != null)
+        {
+            mono.transform.position += cd.bounds.center - position;
+        }
+
+        var cnt = mono.TryCast<CustomNetworkTransform>();
+        if (cnt != null)
+        {
+            cnt.SnapTo(position, (ushort)(cnt.lastSequenceId + 1));
+
+            if (cnt.AmOwner && ModCompatibility.IsSubmerged())
+            {
+                ModCompatibility.ChangeFloor(cnt.myPlayer.GetTruePosition().y > -7);
+                ModCompatibility.CheckOutOfBoundsElevator(cnt.myPlayer);
+            }
+        }
+
+        if (player != null && player.AmOwner)
+        {
+            // If the transported player is a Puppeteer/Parasite controlling someone, snap camera to the victim instead
+            MonoBehaviour? cameraTarget = null;
+            
+            if (player.Data?.Role is ITransportTrigger triggerRole)
+            {
+                cameraTarget = triggerRole.OnTransport();
+            }
+            
+            MiscUtils.SnapPlayerCamera(cameraTarget ?? PlayerControl.LocalPlayer);
+        }
+    }
+}
